36193f60fdda831b653057b49f1f1d8eeccefaff
Jor Sanders <jor.sanders@capgemini.com>
2019-12-23 10:17:56 +0100

    getPosts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private ROOT_URL = 'http://jsonplaceholder.typicode.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPosts(){
+      this.http.get(`${this.ROOT_URL}/posts`);
+  }
 }
