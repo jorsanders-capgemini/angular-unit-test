f9558dbcf0411f0663ca79c4c72cb9ba3490f56c
Jor Sanders <jor.sanders@capgemini.com>
2019-12-23 11:57:35 +0100

    fixed broken tests

--- src/app/components/posts/posts.component.spec.ts
+++ src/app/components/posts/posts.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostsComponent } from './posts.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('PostsComponent', () => {
   let component: PostsComponent;
@@ -8,9 +9,9 @@ describe('PostsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PostsComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule],
+      declarations: [PostsComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
